41802a2c2d7d3d4bab256d50561c8494fae599e4
Nikolay Shubenkov <n.shubenkov@be-interactive.ru>
2015-05-21 16:39:26 +0300

    Initial Commit

---
+++ BlurInactiveScreen-Objective C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlurInactiveScreen-Objective C
+//
+//  Created by Nikolay Shubenkov on 21/05/15.
+//  Copyright (c) 2015 Nikolay Shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlurInactiveScreen-Objective CTests/BlurInactiveScreen_Objective_CTests.m
@@ -0,0 +1,40 @@
+//
+//  BlurInactiveScreen_Objective_CTests.m
+//  BlurInactiveScreen-Objective CTests
+//
+//  Created by Nikolay Shubenkov on 21/05/15.
+//  Copyright (c) 2015 Nikolay Shubenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface BlurInactiveScreen_Objective_CTests : XCTestCase
+
+@end
+
+@implementation BlurInactiveScreen_Objective_CTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
